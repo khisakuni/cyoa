88a851b695a61a2e8e267e36f708ed6ffa2c1f96
Kohei Hisakuni <kohei@philosophie.is>
2017-12-26 17:07:44 -0600

    Refactor

--- story.go
+++ story.go
@@ -1,13 +1,31 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+)
+
+// JSONStory takes reader and decodes json into Story.
+func JSONStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
+// Story is a map of chapter names to Chapters.
 type Story map[string]Chapter
 
+// Chapter struct has Title, Paragraphs slice, and Options slice.
 type Chapter struct {
 	Title      string   `json:"title"`
 	Paragraphs []string `json:"story"`
 	Options    []Option `json:"options"`
 }
 
+// Option Struct has Text and Chapter.
 type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`
